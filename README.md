9d63b18e8c7725fd5c832c73d66c9c73430b3d58
deradam-dotcom <deradam1@gmail.com>
2024-03-08 21:19:14 +0100

    remove some useless icon because the api provide their icon set already, weather response type added based on the api docs later maybe fix some issue around this, api call seted up ready to test

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 ***SETUP***
 
+1. cp .env.sample .env
+2. Add your current WEATHER_API_KEY to your .env file 
+
 ```
 npm ci 
 npm run start
---
+++ src/services/Api.tsx
@@ -0,0 +1,23 @@
+import { Weather } from '../types';
+
+const API_BASE_URL = 'https://api.openweathermap.org/';
+const API_KEY = process.env.WEATHER_API_KEY;
+
+export const getCountryWeatherPosts = async (
+	country: string
+): Promise<Weather> => {
+	try {
+		const response = await fetch(
+			`${API_BASE_URL}data/2.5/weather?q=${country}&appid=${API_KEY}`
+		);
+		if (!response.ok) {
+			throw new Error('Network response was not ok');
+		} else {
+			const data: Weather = await response.json();
+			return data;
+		}
+	} catch (e) {
+		console.error('Error fetching weather data:', e);
+		throw e;
+	}
+};
